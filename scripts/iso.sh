d7ab059c4e85244b5a226351a8d7e4fbf1940fa9
Jakub Sokołowski <jakub@status.im>
2023-12-21 12:05:39 +0100

    allow specifying music path in iso.sh script
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- scripts/iso.sh
+++ scripts/iso.sh
@@ -15,29 +15,33 @@ ISO_CHARSET='utf-8'
 ISO_LABEL='FIRE'
 ISO_PATH="${PWD}/${ISO_LABEL}.iso"
 TMP_DIR=$(mktemp -d '/tmp/playlist_iso.XXXXXXX')
+MUSIC_DIR="${TMP_DIR}"
 CONVERT_MP3=0
 REMOVE_DUPLICATES=0
 
 function show_help() {
   cat << EOF
-Usage: usb_backup.sh [-h|-c|-u] [-C ${ISO_CHARSET}] [-l ${ISO_LABEL}] [-o $(basename ${ISO_PATH})] <PLAYLIST> [PLAYLIST] ...
+Usage: iso.sh [-h|-c|-u] [-C ${ISO_CHARSET}] [-l ${ISO_LABEL}] [-o $(basename ${ISO_PATH})] <PLAYLIST> [PLAYLIST] ...
+
+Creates ISO image from a provided list of playlists. Supports conversion to OGG and deduplication.
 
  -h - Show this help message.
  -c - Convert OGG files to MP3.
  -u - Remove duplicate files.
  -C - ISO image character set.
- -d - Specify loop device path.
- -l - Specify iso image label.
+ -d - Specify dir for music files.
+ -l - Specify ISO image label.
 
 EOF
 }
 
 # Parse arguments
-while getopts "h?c?u?C:l:o:" opt; do
+while getopts "h?c?u?d:C:l:o:" opt; do
   case "$opt" in
     C) ISO_CHARSET="${OPTARG}" ;;
     l) ISO_LABEL="${OPTARG}" ;;
     o) ISO_PATH="${OPTARG}" ;;
+    d) MUSIC_DIR="${OPTARG}" ;;
     c) CONVERT_MP3=1 ;;
     u) REMOVE_DUPLICATES=1 ;;
     h) show_help; exit 0 ;;
@@ -51,33 +55,43 @@ if [[ "${#}" -eq 0 ]]; then
     exit 1
 fi
 
-mkdir -p "${TMP_DIR}"
+mkdir -p "${MUSIC_DIR}"
 # Unmount on exit.
-function cleanup() { rm -fr "${TMP_DIR}"; }
-trap cleanup EXIT ERR INT QUIT
+function cleanup() { rm -fr "${MUSIC_DIR}"; }
+if [[ "${MUSIC_DIR}" == "${TMP_DIR}" ]]; then
+    trap cleanup EXIT ERR INT QUIT
+fi
 
-IDX=1
+function file_exists() {
+    find "${MUSIC_DIR}" -name "${1}" 2>/dev/null | grep -q .
+}
+
+IDX=0
 for PLAYLIST in "${@}"; do
     while read -r -u99 SRC; do
+        IDX=$(( IDX + 1 ))
         NAME="$(basename "../${SRC}")"
         NO_IDX_NAME="$(echo "${NAME}" | sed -e 's/^[0-9]*[ -.)]*//')"
         NAME="$(printf "%03d" "${IDX}") - ${NO_IDX_NAME}"
-        if [[ "${REMOVE_DUPLICATES}" -eq 1 ]]; then
-            if find "${TMP_DIR}" -name "* - ${NO_IDX_NAME}" 2>/dev/null | grep -q .; then
-                echo -e "${RED}SKIPDUP:${RST} ${BLD}${NAME}${RST}"
-                continue
-            fi
+        # Avoid copying and converting again the same files
+        if [[ -f "${MUSIC_DIR}/${NAME%.[mp3ogg]*}.mp3" ]] ||
+           [[ -f "${MUSIC_DIR}/${NAME%.[mp3ogg]*}.ogg" ]]; then
+            echo -e "${GRN}SKIPPED:${RST} ${BLD}${NAME}${RST}"
+            continue
+        elif [[ "${REMOVE_DUPLICATES}" -eq 1 ]] && file_exists "* - ${NO_IDX_NAME}"; then
+            echo -e "${RED}SKIPDUP:${RST} ${BLD}${NAME}${RST}"
+            IDX=$(( IDX - 1 ))
+            continue
         fi
         if [[ "${CONVERT_MP3}" -eq 1 ]] && [[ "${NAME}" =~ .ogg$ ]]; then
             NAME="${NAME%.ogg}.mp3"
             echo -e "${YLW}CONVERT:${RST} ${BLD}${NAME}${RST}"
             # Some players do not support OGG.
-            ffmpeg -loglevel error -i "../${SRC}" "${TMP_DIR}/${NAME}"
+            ffmpeg -loglevel error -i "../${SRC}" "${MUSIC_DIR}/${NAME}"
         else
             echo -e "${GRN}COPYING:${RST} ${BLD}${NAME}${RST}"
-            cp "../${SRC}" "${TMP_DIR}/${NAME}"
+            cp "../${SRC}" "${MUSIC_DIR}/${NAME}"
         fi
-        IDX=$(( IDX + 1 ))
     done 99< "${PLAYLIST}"
 done
 
@@ -85,20 +99,28 @@ echo
 if [[ "${REMOVE_DUPLICATES}" -eq 0 ]]; then
     echo -e "${BLD}Checking for duplicates.." >&2
     echo
-    if fdupes -r "${TMP_DIR}"; then
+    if fdupes -r "${MUSIC_DIR}"; then
         echo -e "${YLW}WARNING:${RST} Duplicates present!" >&2
         echo
     fi
 fi
 
+
+echo -e "${BLD}Music folder size:${RST}"
+echo
+sync
+du -hs "${MUSIC_DIR}"
+echo
 echo -e "${BLD}Creating ISO image...${RST}"
+
 mkisofs -quiet \
     -output="${ISO_PATH}" \
     -volid="${ISO_LABEL}" \
     -input-charset="${ISO_CHARSET}" \
-    "${TMP_DIR}"
+    "${MUSIC_DIR}"
 
 echo
-du -hs "${ISO_PATH}"
+sync
+du -h "${ISO_PATH}"
 echo
 echo -e "${GRN}SUCCESS!${RST}"
