e9486fd7554252f05b6864f9d50accc40b1cd0fc
Jakub Sokołowski <jakub@status.im>
2023-12-21 11:39:54 +0100

    add deuplication to iso.sh script
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- scripts/iso.sh
+++ scripts/iso.sh
@@ -1,42 +1,45 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash -p cdrkit -p ffmpeg
+#!nix-shell -i bash -p cdrkit -p ffmpeg -p fdupes
 # Script for creating an ISO image from playlist(s).
 set -euo pipefail
 
 # Colors
-export YLW='\033[1;33m'
-export RED='\033[0;31m'
-export GRN='\033[0;32m'
-export BLU='\033[0;34m'
 export BLD='\033[1m'
 export RST='\033[0m'
+export YLW="\033[1;33m${BLD}"
+export RED="\033[0;31m${BLD}"
+export GRN="\033[0;32m${BLD}"
+export BLU="\033[0;34m${BLD}"
 
 ISO_CHARSET='utf-8'
 ISO_LABEL='FIRE'
-ISO_PATH="${ISO_LABEL}.iso"
+ISO_PATH="${PWD}/${ISO_LABEL}.iso"
 TMP_DIR=$(mktemp -d '/tmp/playlist_iso.XXXXXXX')
 CONVERT_MP3=0
+REMOVE_DUPLICATES=0
 
 function show_help() {
   cat << EOF
-Usage: usb_backup.sh [-h|-c] [-C ${ISO_CHARSET}] [-l ${ISO_LABEL}] [-o ${ISO_PATH}] <PLAYLIST> [PLAYLIST] ...
+Usage: usb_backup.sh [-h|-c|-u] [-C ${ISO_CHARSET}] [-l ${ISO_LABEL}] [-o $(basename ${ISO_PATH})] <PLAYLIST> [PLAYLIST] ...
 
  -h - Show this help message.
+ -c - Convert OGG files to MP3.
+ -u - Remove duplicate files.
  -C - ISO image character set.
  -d - Specify loop device path.
  -l - Specify iso image label.
- -c - Convert OGG files to MP3.
 
 EOF
 }
 
 # Parse arguments
-while getopts "C:l:o:c?h?" opt; do
+while getopts "h?c?u?C:l:o:" opt; do
   case "$opt" in
     C) ISO_CHARSET="${OPTARG}" ;;
     l) ISO_LABEL="${OPTARG}" ;;
     o) ISO_PATH="${OPTARG}" ;;
     c) CONVERT_MP3=1 ;;
+    u) REMOVE_DUPLICATES=1 ;;
     h) show_help; exit 0 ;;
     *) show_help; exit 1 ;;
   esac
@@ -57,12 +60,18 @@ IDX=1
 for PLAYLIST in "${@}"; do
     while read -r -u99 SRC; do
         NAME="$(basename "../${SRC}")"
-        NAME="$(echo "${NAME}" | sed -e 's/^[0-9]*[ -.)]*//')"
-        NAME="$(printf "%03d" "${IDX}") - ${NAME}"
-        # Some players do not support OGG.
+        NO_IDX_NAME="$(echo "${NAME}" | sed -e 's/^[0-9]*[ -.)]*//')"
+        NAME="$(printf "%03d" "${IDX}") - ${NO_IDX_NAME}"
+        if [[ "${REMOVE_DUPLICATES}" -eq 1 ]]; then
+            if find "${TMP_DIR}" -name "* - ${NO_IDX_NAME}" 2>/dev/null | grep -q .; then
+                echo -e "${RED}SKIPDUP:${RST} ${BLD}${NAME}${RST}"
+                continue
+            fi
+        fi
         if [[ "${CONVERT_MP3}" -eq 1 ]] && [[ "${NAME}" =~ .ogg$ ]]; then
             NAME="${NAME%.ogg}.mp3"
             echo -e "${YLW}CONVERT:${RST} ${BLD}${NAME}${RST}"
+            # Some players do not support OGG.
             ffmpeg -loglevel error -i "../${SRC}" "${TMP_DIR}/${NAME}"
         else
             echo -e "${GRN}COPYING:${RST} ${BLD}${NAME}${RST}"
@@ -73,6 +82,15 @@ for PLAYLIST in "${@}"; do
 done
 
 echo
+if [[ "${REMOVE_DUPLICATES}" -eq 0 ]]; then
+    echo -e "${BLD}Checking for duplicates.." >&2
+    echo
+    if fdupes -r "${TMP_DIR}"; then
+        echo -e "${YLW}WARNING:${RST} Duplicates present!" >&2
+        echo
+    fi
+fi
+
 echo -e "${BLD}Creating ISO image...${RST}"
 mkisofs -quiet \
     -output="${ISO_PATH}" \
@@ -80,4 +98,7 @@ mkisofs -quiet \
     -input-charset="${ISO_CHARSET}" \
     "${TMP_DIR}"
 
-du -hsc "${ISO_PATH}"
+echo
+du -hs "${ISO_PATH}"
+echo
+echo -e "${GRN}SUCCESS!${RST}"
